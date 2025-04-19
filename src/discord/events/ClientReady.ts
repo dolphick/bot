96242059339b9d0379dc708bb57f6d0364fa00c0
dolphick <107743491+dolphick@users.noreply.github.com>
2025-04-20 02:51:56 +0900

    起こしてすぐだとまだ接続できてないので

--- src/discord/events/ClientReady.ts
+++ src/discord/events/ClientReady.ts
@@ -7,7 +7,6 @@ module.exports = {
 	async execute(client: Client<true>) {
 		await client.rest.put(Routes.applicationCommands(client.application.id) ,{body:discord.commands.rest});
 		console.log("[Discord] Ready");
-		discord.stats.set(Math.trunc(new Date().getTime()/1000), client.ws.ping);
 		setInterval(() => {
 			discord.stats.set(Math.trunc(new Date().getTime()/1000), client.ws.ping);
 		}, 60000)	  
